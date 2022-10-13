76057d6e7220fa1e7a9e0e3ea2db2ff922ee5b89
choeqq <k.chola@op.pl>
2022-10-13 22:05:20 +0200

    encrypt, decrypt vault

--- client/src/components/Vault.tsx
+++ client/src/components/Vault.tsx
@@ -1,5 +1,101 @@
-function Vault() {
-  return <div>Vault</div>;
+import { Box, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { useFieldArray, useForm } from "react-hook-form";
+import { encryptVault } from "../crypto";
+import { VaultItem } from "../pages";
+import FormWrapper from "./FormWrapper";
+
+function Vault({
+  vault: [],
+  vaultKey = "",
+}: {
+  vault: VaultItem[];
+  vaultKey: string;
+}) {
+  const { control, register, handleSubmit } = useForm();
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "vault",
+  });
+
+  return (
+    <FormWrapper
+      onSubmit={handleSubmit(({ vault }) => {
+        const encryptedVault = encryptVault({
+          vault: JSON.stringify({ vault }),
+          vaultKey,
+        });
+
+        window.sessionStorage.setItem("vault", JSON.stringify(vault));
+      })}
+    >
+      {fields.map((field, index) => {
+        return (
+          <Box
+            key={field.id}
+            display="flex"
+            alignItems="flex-end"
+            mt="4"
+            mb="4"
+          >
+            <FormControl>
+              <FormLabel htmlFor="website">Website</FormLabel>
+              <Input
+                type="url"
+                id="website"
+                placeholder="website"
+                {...register(`Vault.${index}.website`, {
+                  required: "Website is required",
+                })}
+              />
+            </FormControl>
+            <FormControl ml="2">
+              <FormLabel htmlFor="username">Username</FormLabel>
+              <Input
+                id="username"
+                placeholder="Username"
+                {...register(`Vault.${index}.username`, {
+                  required: "Username is required",
+                })}
+              />
+            </FormControl>
+            <FormControl ml="2">
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <Input
+                type="password"
+                id="password"
+                placeholder="Password"
+                {...register(`Vault.${index}.password`, {
+                  required: "Password is required",
+                })}
+              />
+            </FormControl>
+
+            <Button
+              type="button"
+              bg="red.500"
+              color="white"
+              fontSize="2xl"
+              ml="2"
+              onClick={() => remove(index)}
+            >
+              -
+            </Button>
+          </Box>
+        );
+      })}
+
+      <Button
+        onClick={() => append({ website: "", username: "", password: "" })}
+      >
+        Add
+      </Button>
+
+      <Button type="submit" color="teal" ml="8">
+        Save Vault
+      </Button>
+    </FormWrapper>
+  );
 }
 
 export default Vault;
--- client/src/crypto.ts
+++ client/src/crypto.ts
@@ -1,5 +1,5 @@
 import pbkdf2 from "crypto-js/pbkdf2";
-import { SHA256 } from "crypto-js";
+import { AES, enc, SHA256 } from "crypto-js";
 
 export function hashPassword(password: string) {
   return SHA256(password).toString();
@@ -17,4 +17,31 @@ export function generateVaultKey({
   return pbkdf2(`${email}:${hashedPassword}`, salt, {
     keySize: 32,
   }).toString();
+}
+
+export function decryptVault({
+  vaultKey,
+  vault,
+}: {
+  vaultKey: string;
+  vault: string;
+}) {
+  const bytes = AES.decrypt(vault, vaultKey);
+  const decrypted = bytes.toString(enc.Utf8);
+
+  try {
+    return JSON.parse(decrypted).vault;
+  } catch (e) {
+    return null;
+  }
+}
+
+export function encryptVault({
+  vaultKey,
+  vault,
+}: {
+  vaultKey: string;
+  vault: string;
+}) {
+  return AES.encrypt(vault, vaultKey).toString();
 }
